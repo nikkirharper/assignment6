0476b144e285416e944e45cdeaea11c67866a11f
Nikki Harper <nicoleharper@ufl.edu>
2020-04-25 12:33:38 -0600

    Adds members array, tour object and while loop

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+var members = [
+    'Dave Grohl',
+    'Nate Mendel',
+    'Pat Smear',
+    'Taylor Hawkins',
+    'Rami Jaffee',
+    'Chris Shiflett'
+];
+
+var tour = {
+  Chicago: "July 29 & 30, 2018",
+  Seattle: "September 1, 2018",
+  Edmonton: "September 4, 2018"
+};
+
+var x= 1;
+
+while (x < 6) {
+  console.log('Times running through the loop: ' + x);
+
+  x++;
+}
